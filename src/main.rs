e58c51120ca9f964d0cc842f364724e7a84c7cd6
LeonardoCavazzola <leo.cavazzola@hotmail.com>
2022-09-09 13:01:36 -0300

    structs

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,33 @@
+use crate::structs::Account;
+use crate::structs::User;
+
 mod error;
 mod conditional;
 mod loops;
 mod memory;
+mod structs;
 
 fn main() {
-    println!("hello");
+    // structs::run();
+    let user = User::new(String::from("Leonardo"));
+    let account = Account::new(user, 54.0);
+
+    println!("{}", account);
+    let vo = account.to_vo();
+    println!("{}, {}", vo.name, vo.value);
+    println!("{}", account);
 }
 
+impl Account {
+    pub fn to_vo(&self) -> AccountVo {
+        return AccountVo {
+            name: Box::from(self.user().name()),
+            value: Box::from(self.value()),
+        };
+    }
+}
+
+pub struct AccountVo {
+    pub name: Box<str>,
+    pub value: Box<f64>,
+}
